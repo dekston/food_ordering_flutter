c62f87593cba23e34892d6cfd676dad476e4a598
dekston <dekstonfung@gmail.com>
2022-04-11 17:55:31 +0800

    food_ordering_1541

---
+++ lib/home/main_foot_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  @override
+  _MainFoodPageState createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Container(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Text('Country'),
+                Text('City'),
+              ],
+            ),
+            Container(
+              width: 45,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
